cc0da9c8d119044f837f4116bb6c2232c9565f32
jtingsanchali <70384906+joe-tingsanchali-sonarsource@users.noreply.github.com>
2023-03-25 14:49:09 -0500

    Update file.js

--- src/file.js
+++ src/file.js
@@ -15,6 +15,7 @@ aorgoieoi=0;
 aoiio34io=0;
 aoweif23=0;
 aowiejfioaejw=9;
+aweoifj=29892;
 
 
 helloFromFeatureTestRemoveTwoBugs="what";
@@ -22,15 +23,6 @@ bugFrom101=0;
 
 bugFrom102a=0;
 bugFrom102b=0;
-
-bugFrom103a=0;
-bugFrom103b=0;
-bugFrom103c=0;
-
-bugFrom104a=0;
-bugFrom104b=0;
-bugFrom104c=0;
-bugFrom104d=0;
 
 let keyDownEvnt;
 keyDownEvnt = '';
